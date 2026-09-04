9d723c0e6a8ffb185b142d548ce06d348928e2bb
hejmua <staffdeveloper.discord.com@gmail.com>
2026-09-04 12:50:30 +0200

    fix(image): enable services without SysV helper

--- tests/integration/image-sanity.sh
+++ tests/integration/image-sanity.sh
@@ -6,6 +6,10 @@ test -s /etc/kairos-release
 test -x /opt/dead-rose/bin/dead-rose-core
 test -x /opt/dead-rose/bin/dead-rose-shell
 test -f /etc/systemd/system/dead-rose-core.service
+test "$(readlink /etc/systemd/system/multi-user.target.wants/dead-rose-core.service)" = /etc/systemd/system/dead-rose-core.service
+test "$(readlink /etc/systemd/system/multi-user.target.wants/NetworkManager.service)" = /usr/lib/systemd/system/NetworkManager.service
+test "$(readlink /etc/systemd/system/multi-user.target.wants/greetd.service)" = /usr/lib/systemd/system/greetd.service
+test "$(readlink /etc/systemd/system/display-manager.service)" = /usr/lib/systemd/system/greetd.service
 test -f /etc/greetd/config.toml
 grep -Fq 'user = "deadrose"' /etc/greetd/config.toml
 awk -F: '$2 !~ /^(!|\*)/ { exit 1 }' /etc/shadow
